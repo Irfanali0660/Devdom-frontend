51a105530b9248c235e1d81c89277904b3976b6b
irfanali <irfu3844@gmail.com>
2023-05-17 16:02:48 +0530

    google

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,9 @@
 export const environment = {
+    // socialAuth:{
+    //     googleKey:'1017251737101-2n4k38bn6rh9i70ihlc5vcb7ftfji34b.apps.googleusercontent.com'
+    // },
     socialAuth:{
-        googleKey:'1017251737101-2n4k38bn6rh9i70ihlc5vcb7ftfji34b.apps.googleusercontent.com'
+        googleKey:'571741001580-hcgtdlfa9dsdeju02vm6cfudkqkmiv37.apps.googleusercontent.com'
     },
     socketUrl: 'https://devbackend.irfanali.club', 
      mapTiler: {
